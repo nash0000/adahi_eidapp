03175d58ebf6d0ab6f60c9bedfc7182f4cf8829c
nash <nashmiyya8@gmail.com>
2021-06-09 04:31:56 +0300

    added all the constant for the collections

--- lib/shared/app_strings.dart
+++ lib/shared/app_strings.dart
@@ -7,3 +7,35 @@ const String kUserEmail = 'userEmail';
 const String kUserPassword = 'userPassword';
 const String kUserAddress = 'userAddress';
 const String kUserPhone = 'userPhone';
+
+const String kButcherCollectionName = 'butchers';
+const String kButcherShopName = 'butcherShopName';
+const String kButcherID = 'ButcherID';
+const String kButcherEmail = 'butcherEmail';
+const String kButcherPassword = 'butcherPassword';
+const String kButcherShopAddress = 'butcherAddress';
+const String kButcherPhone = 'butcherPhone';
+const String kButcherArea = 'area';
+
+const String kAreaCollectionName = 'areas';
+const String kAreaName = 'areaName';
+const String kAreaID = 'areaID';
+
+const String kMeatCollectionName = 'meats';
+const String kMeatType = 'meatType';
+const String kMeatID = 'meatID';
+const String kMeatPrice = 'meatPrice';
+const String kMeatService = 'meatService';
+
+const String kOrderCollectionName = 'orders';
+const String kOrderID = 'orderID';
+const String kAvailableMeatID = 'availableMeatID';
+const String kOrderAddress = 'orderAddress';
+const String kOrderTime = 'orderTime';
+const String kOrderTotalPrice = 'orderTotalPrice';
+
+const String kAvailableMeatsCollectionName = 'availableMeats';
+const String kAvailableMeatIDd = 'availableMeatID';
+const String kAvailableMeatButcherID = 'butcherID';
+const String kAvailableMeatTotalPrice = 'meatPrice';
+const String kAvailableMeat = 'meatID';
